98c34153ae7e0fb9f9d28f50cd91eb347b515684
LuisMejiasR <luisme.1993@gmail.com>
2025-01-07 11:33:20 -0300

    feat: adding service to controller and creating first endpoint

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Post, Get, Put, Delete, Res, HttpStatus, Body } from '@nestjs/common';
-
 import { CreateProductDTO } from "./dto/product.dto";
+import { ProductService } from './product.service';
+
 @Controller('product')
 export class ProductController {
 
+    constructor(private productService: ProductService) {}
+
     @Post("/create")
-    createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+    async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
         // console.log(createProductDTO);
+        const product = await this.productService.createProduct(createProductDTO)
         return res.status(HttpStatus.OK).json({
-            message: "recieved"
+            message: "recieved",
+            product: product
         });
     }
 
