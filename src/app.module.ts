85f50e56fe018d6bcb0f952a13b7cb3bdfe84e77
LuisMejiasR <luisme.1993@gmail.com>
2025-01-07 11:03:09 -0300

    feat: adding mongodb, mongoose and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://luisme1993:e4FmnaNFejNzCaEo@cluster0.zuach.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
   controllers: [AppController],
   providers: [AppService],
 })
